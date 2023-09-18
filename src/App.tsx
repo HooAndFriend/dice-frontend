db1e5473f39536ce133b6fcda8afb3b46bee75fa
pinomaker-hoo <inhoo23@naver.com>
2023-09-19 00:38:04 +0900

    feat : Set Context

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,33 @@ import Dashboard from './pages/dashboard-page'
 import SignupPage from './pages/signup-page'
 import SocialSignupPage from './pages/social-signup-page'
 
+// ** Context Imports
+import { ErrorProvider } from './context/ErrorContext'
+import { DialogProvider } from './context/DialogContext'
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PublicRoute />}>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/social/signup" element={<SocialSignupPage />} />
-        </Route>
-        <Route element={<ProtectRoute />}>
-          <Route path="/dashboard/*" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ErrorProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/social/signup" element={<SocialSignupPage />} />
+          </Route>
+          <Route element={<ProtectRoute />}>
+            <Route
+              path="/dashboard/*"
+              element={
+                <DialogProvider>
+                  <Dashboard />
+                </DialogProvider>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ErrorProvider>
   )
 }
 
--- src/context/ErrorContext.tsx
+++ src/context/ErrorContext.tsx
@@ -1,36 +1,36 @@
-import { useState, createContext, useContext } from "react";
-import { Alert } from "components/Alert";
+import { useState, createContext, useContext } from 'react'
+import { Alert } from '@/components/Alert'
 
 interface ContextProps {
-  onError(title: string, contents: string): void;
+  onError(title: string, contents: string): void
 }
 
-const Context = createContext<ContextProps>({} as ContextProps);
+const Context = createContext<ContextProps>({} as ContextProps)
 
 export function ErrorProvider({ children }: { children: React.ReactNode }) {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
   const [alertContents, setAlertContents] = useState({
-    title: "",
-    contents: "",
-  });
+    title: '',
+    contents: '',
+  })
 
   const onClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const onError = (title: string, contents: string) => {
-    setAlertContents({ title, contents });
-    setOpen(true);
-  };
+    setAlertContents({ title, contents })
+    setOpen(true)
+  }
 
   return (
     <Context.Provider value={{ onError }}>
       <Alert open={open} {...alertContents} type="alert" onClose={onClose} />
       {children}
     </Context.Provider>
-  );
+  )
 }
 
 export function useError() {
-  return useContext(Context);
+  return useContext(Context)
 }
