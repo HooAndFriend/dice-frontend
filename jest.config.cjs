36da937f53af5638b04aff900fccd19eaaea687a
pinomaker-hoo <inhoo23@naver.com>
2023-09-22 02:51:33 +0900

    feat : Add Jest Set

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  preset: 'ts-jest',
+}
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+// Imports
+import { render, screen, fireEvent } from '@testing-library/react'
+
+// To Test
+import App from '../App'
+
+// Tests
+test('Renders main page correctly', async () => {
+  // Setup
+  render(<App />)
+})
