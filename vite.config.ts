7546f9c655fa21f3f1092363f622d88f3762698f
pinomaker-hoo <inhoo23@naver.com>
2023-10-04 15:13:28 +0900

    del : no use code

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import svgr from 'vite-plugin-svgr'
 
 const __dirname = process.cwd()
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
