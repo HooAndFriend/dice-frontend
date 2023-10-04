98b4675d9293b8846b761f6c2ff77f8b4c260dd4
pinomaker-hoo <inhoo23@naver.com>
2023-10-04 14:58:56 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import svgr from 'vite-plugin-svgr'
 
 const __dirname = process.cwd()
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
