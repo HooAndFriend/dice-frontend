45484a4cfe05433b2f4638c1db0bf66dc4515a95
pinomaker-hoo <inhoo23@naver.com>
2023-11-11 13:35:46 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 // ** Router Imports
-import { Routes, Route } from 'react-router-dom'
-import { ProtectRoute, PublicRoute } from './utils/protect-route'
+import { Route, Routes } from 'react-router-dom'
 
 // ** Router Imports
-import LoginPage from './pages/login-page'
 import Dashboard from './pages/dashboard-page'
+import LoginPage from './pages/login-page'
 import SignupPage from './pages/signup-page'
 import SocialSignupPage from './pages/social-signup-page'
 
 // ** Context Imports
-import { ErrorProvider } from './context/ErrorContext'
 import { DialogProvider } from './context/DialogContext'
+import { ErrorProvider } from './context/ErrorContext'
 import { WorkspaceProvider } from './context/WorkspaceContext'
 
 // ** Style Imports
-import { ThemeProvider } from '@mui/material'
 import { theme } from '@/theme'
-import { GlobalStyles } from '@mui/material'
+import { GlobalStyles, ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 
 const App = () => {
@@ -26,23 +24,23 @@ const App = () => {
       <CssBaseline />
       <ErrorProvider>
         <Routes>
-          <Route element={<PublicRoute />}>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/social/signup" element={<SocialSignupPage />} />
-          </Route>
-          <Route element={<ProtectRoute />}>
-            <Route
-              path="/dashboard/*"
-              element={
-                <WorkspaceProvider>
-                  <DialogProvider>
-                    <Dashboard />
-                  </DialogProvider>
-                </WorkspaceProvider>
-              }
-            />
-          </Route>
+          {/* <Route element={<PublicRoute />}> */}
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/social/signup" element={<SocialSignupPage />} />
+          {/* </Route> */}
+          {/* <Route element={<ProtectRoute />}> */}
+          <Route
+            path="/dashboard/*"
+            element={
+              <WorkspaceProvider>
+                <DialogProvider>
+                  <Dashboard />
+                </DialogProvider>
+              </WorkspaceProvider>
+            }
+          />
+          {/* </Route> */}
         </Routes>
       </ErrorProvider>
     </ThemeProvider>
