57e66f41bf8cacf803d94cf948551b645d68171a
pinomaker-hoo <inhoo23@naver.com>
2023-11-14 01:20:38 +0900

    fix : Content box

--- src/layouts/content/index.tsx
+++ src/layouts/content/index.tsx
@@ -9,7 +9,6 @@ const ContentBox = ({ children }) => {
         width: '100%',
         height: '100vh',
         backgroundColor: Color.smallPurple,
-        borderRadius: 30,
       }}
     >
       {children}
