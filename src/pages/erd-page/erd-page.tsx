c8c439d80ad779c624f38d91a0a8c80917c815b0
pinomaker-hoo <inhoo23@naver.com>
2023-11-14 01:20:16 +0900

    feat : Erd Layout

--- src/pages/erd-page/erd-page.tsx
+++ src/pages/erd-page/erd-page.tsx
@@ -1,5 +1,32 @@
+// ** Mui Imports
+import { Box } from '@mui/material'
+
+// ** Constant Imports
+import Color from '@/constants/color'
+
+// ** Component Imports
+import { ContextBox } from '@/components/ContentBox'
+import SideMenu from './sideMenu'
+
 const ErdPageView = () => {
-  return <h1>ERD Page</h1>
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        backgroundColor: Color.purple,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{ width: '20%' }}>
+        <SideMenu />
+      </Box>
+      <ContextBox>
+        <Box></Box>
+      </ContextBox>
+    </Box>
+  )
 }
 
 export default ErdPageView
