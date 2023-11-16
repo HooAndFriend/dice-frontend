0251082b452b4ccd9547bb377c7aecdc5e17e6b4
pinomaker-hoo <inhoo23@naver.com>
2023-11-16 15:25:47 +0900

    fix : Home Page

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -6,6 +6,10 @@ import type { WorkspaceV1 } from '@/types/workspace'
 
 // ** Component Imports
 import ImageBox from '@/components/Image'
+import { ContextBox } from '@/components/ContentBox'
+
+// ** Constant Imports
+import Color from '@/constants/color'
 
 interface PropsType {
   worksapce: WorkspaceV1
@@ -16,29 +20,33 @@ const HomePageView = ({ worksapce }: PropsType) => {
     <Box
       sx={{
         display: 'flex',
-        mt: 10,
-        ml: 10,
+        backgroundColor: Color.purple,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
       }}
     >
-      <Box>
-        <ImageBox
-          image={worksapce.profile}
-          width={500}
-          height={500}
-          alt="workspace profile"
-        />
-      </Box>
-      <Box sx={{ ml: 10, mt: 5 }}>
-        <Typography variant="h1" sx={{ color: 'black' }}>
-          Welcome to
-        </Typography>
-        <Typography variant="h1" sx={{ color: 'black' }}>
-          {worksapce.name}
-        </Typography>
-        <Typography variant="body1" sx={{ color: 'black', mt: 5 }}>
-          {worksapce.comment}
-        </Typography>
-      </Box>
+      <ContextBox type="ALL">
+        <Box>
+          <ImageBox
+            image={worksapce.profile}
+            width={500}
+            height={500}
+            alt="workspace profile"
+          />
+        </Box>
+        <Box sx={{ ml: 10, mt: 5 }}>
+          <Typography variant="h1" sx={{ color: 'black' }}>
+            Welcome to
+          </Typography>
+          <Typography variant="h1" sx={{ color: 'black' }}>
+            {worksapce.name}
+          </Typography>
+          <Typography variant="body1" sx={{ color: 'black', mt: 5 }}>
+            {worksapce.comment}
+          </Typography>
+        </Box>
+      </ContextBox>
     </Box>
   )
 }
