eed48a1b34d0da08ac7ae19d1714c4c4fa51e4d9
최혜림 <chhari0708@naver.com>
2023-11-30 21:11:59 +0900

    index.ts 생성

--- src/pages/erd-page/index.tsx
+++ src/pages/erd-page/index.tsx
@@ -1,7 +1,44 @@
 // ** Component Imports
+import { useLazyGetTableQuery } from '@/services'
 import ErdPageView from './erd-page'
+import useInput from '@/hooks/useInput'
+import { Table } from '@/types/erd'
+import { useWorkspace } from '@/context/WorkspaceContext'
+import { useEffect } from 'react'
 
 const ErdPage = () => {
+  const {
+    data: table,
+    setData: setTable,
+    handleInput,
+  } = useInput<Table>({
+    id: 0,
+    name: '',
+    comment: '',
+    column: [],
+    createUser: {
+      nickname: '',
+      email: '',
+      profile: '',
+    },
+    modifyUser: {
+      nickname: '',
+      email: '',
+      profile: '',
+    },
+  })
+  const { workspaceId } = useWorkspace()
+  const [getTableApi] = useLazyGetTableQuery()
+
+  useEffect(() => {
+    getTableApi(workspaceId)
+      .unwrap()
+      .then((res) => {
+        console.log(res.data.findErd)
+        setTable(res.data.findErd)
+      })
+  }, [workspaceId])
+
   return <ErdPageView />
 }
 
