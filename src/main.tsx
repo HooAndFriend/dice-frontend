dc54f2b8d33ddeb601ae8767268600c89358bd1b
pinomaker-hoo <inhoo23@naver.com>
2023-12-04 17:44:43 +0900

    feat : Add Set tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,8 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 
+import './style/index.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
